44c1e5b119701978a1b8d067cdc6f2559ff69f0f
Amed Salazar Estrella <amed157@hotmail.com>
2024-10-28 15:47:16 -0500

    refactor code

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'dashboard#index'
   resources :sign_in, only: [:new, :create]
-  resources :users, only: [:index, :new, :create]
+  resources :user_profiles, only: [:index, :edit, :update]
   resources :dashboard, only: [:index, :new, :create]
   resources :consultations, only: [:index, :new, :create]
 end
